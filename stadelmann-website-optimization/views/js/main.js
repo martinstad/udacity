34beee93aed39af7349343fa7ec1f9ba3bf284ad
martinstad <30287728+martinstad@users.noreply.github.com>
2018-05-13 15:30:15 +0200

    Update main.js

--- stadelmann-website-optimization/views/js/main.js
+++ stadelmann-website-optimization/views/js/main.js
@@ -586,5 +586,5 @@ document.addEventListener('DOMContentLoaded', function() {
         elem.style.top = (Math.floor(i / cols) * s) + 'px';
         document.querySelector("#movingPizzas1").appendChild(elem);
     }
-    updatePositions();
+    //updatePositions();
 });
