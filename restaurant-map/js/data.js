d263b7f1f331eea291575c7970d13a6f7fef8981
martinstad <30287728+martinstad@users.noreply.github.com>
2018-06-06 10:04:09 +0200

    Add files via upload

---
+++ restaurant-map/js/data.js
@@ -0,0 +1,58 @@
+var locations = [{
+        title: "Bebek",
+        location: {
+            lat: 47.374499,
+            lng: 8.520690
+        },
+    },
+    {
+        title: "Didi's Frieden",
+        location: {
+            lat: 47.379333,
+            lng: 8.543476
+        },
+    },
+    {
+        title: "Fischer's Fritz",
+        location: {
+            lat: 47.335746,
+            lng: 8.541247
+        },
+
+    },
+    {
+        title: "Gartenhof",
+        location: {
+            lat: 47.371911,
+            lng: 8.525576
+        },
+    },
+    {
+        title: "Giesserei",
+        location: {
+            lat: 47.408567,
+            lng: 8.537670
+        },
+    },
+    {
+        title: "Les Halles",
+        location: {
+            lat: 47.387888,
+            lng: 8.518644
+        },
+    },
+    {
+        title: "Maison Blunt",
+        location: {
+            lat: 47.383701,
+            lng: 8.528404
+        },
+    },
+    {
+        title: "Rosso",
+        location: {
+            lat: 47.385600,
+            lng: 8.518322
+        },
+    }
+];
